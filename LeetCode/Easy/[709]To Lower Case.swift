fe05fdcb17404363941cd38b3762500f178ffd70
Archie <Archie@Archie.tw>
2020-04-20 08:23:18 +0800

    feat(easy): add 709 To Lower Case

---
+++ LeetCode/Easy/[709]To Lower Case.swift
@@ -0,0 +1,11 @@
+/*
+ Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ */
+
+import Foundation
+
+extension Easy {
+    static func toLowerCase(_ str: String) -> String {
+        str.lowercased()
+    }
+}
