03c6f30e5a2a50e51e78f10c262454a8ec7fdca4
Archie <Archie@Archie.tw>
2020-04-21 14:43:30 +0800

    feat(easy): add 771 Jewels and Stones

---
+++ LeetCode/Easy/[771]Jewels and Stones.swift
@@ -0,0 +1,15 @@
+/*
+ You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+ The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ */
+
+import Foundation
+
+extension Easy {
+    static func numJewelsInStones(_ J: String, _ S: String) -> Int {
+        S.reduce(0) { result, character -> Int in
+            J.contains(character) ? result + 1 : result
+        }
+    }
+}
