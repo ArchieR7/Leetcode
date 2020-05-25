00c82378c9916b3fe11495759080e5de9be5fabb
Archie <Archie@Archie.tw>
2020-05-25 13:02:09 +0800

    feat(easy): add 1281 - Subtract the Product and Sum of Digits of an Integer

---
+++ LeetCode/Easy/[1281]Subtract the Product and Sum of Digits of an Integer.swift
@@ -0,0 +1,43 @@
+/*
+ Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+  
+
+ Example 1:
+
+ Input: n = 234
+ Output: 15
+ Explanation:
+ Product of digits = 2 * 3 * 4 = 24
+ Sum of digits = 2 + 3 + 4 = 9
+ Result = 24 - 9 = 15
+ Example 2:
+
+ Input: n = 4421
+ Output: 21
+ Explanation:
+ Product of digits = 4 * 4 * 2 * 1 = 32
+ Sum of digits = 4 + 4 + 2 + 1 = 11
+ Result = 32 - 11 = 21
+  
+
+ Constraints:
+
+ 1 <= n <= 10^5
+
+ */
+
+import Foundation
+
+extension Easy {
+    static func subtractProductAndSum(_ n: Int) -> Int {
+        let s: String = n.description
+        var product: Int = 1
+        var sum: Int = 0
+        s.forEach { (c) in
+            guard let i: Int = Int(String(c)) else { return }
+            product *= i
+            sum += i
+        }
+        return product - sum
+    }
+}
